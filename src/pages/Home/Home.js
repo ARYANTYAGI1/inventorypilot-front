b37d4555dad2993608abe01f39da25efbf0c8a13
Aryan Tyagi <aryan.tyagi@huulke.com>
2024-05-30 17:36:52 +0530

    first commit for frontend

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { RiProductHuntLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import "./Home.scss";
+import heroImg from "../../assets/inv-img.png";
+import { ShowOnLogin, ShowOnLogout } from "../../components/protect/HiddenLink";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <nav className="container --flex-between ">
+        <div className="logo"  style={{fontSize: '22px'}}>
+          InventoryPilot
+        </div>
+
+        <ul className="home-links">
+          <ShowOnLogout>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ShowOnLogout>
+          <ShowOnLogout>
+            <li>
+              <button className="--btn --btn-primary">
+                <Link to="/login">Login</Link>
+              </button>
+            </li>
+          </ShowOnLogout>
+          <ShowOnLogin>
+            <li>
+              <button className="--btn --btn-primary">
+                <Link to="/dashboard">Dashboard</Link>
+              </button>
+            </li>
+          </ShowOnLogin>
+        </ul>
+      </nav>
+      {/* HERO SECTION */}
+      <section className="container hero">
+        <div className="hero-text">
+          <h2>Inventory {"&"} Stock Management Solution</h2>
+          <p>
+            Inventory system to control and manage proucts in the warehouse in
+            real timeand integrated to make it easier to develop your business.
+          </p>
+        <div className="--flex-start">
+          <NumberText text="Growing Community" num="Get Started Today" />
+        </div>
+        </div>
+
+        <div className="hero-image">
+          <img src={heroImg} alt="Inventory" />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+const NumberText = ({ num, text }) => {
+  return (
+    <div className="--mr">
+      <h3 className="--color-white">{num}</h3>
+      <p className="--color-white">{text}</p>
+    </div>
+  );
+};
+
+export default Home;
